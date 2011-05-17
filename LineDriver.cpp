64fd4d3a705fb9b1b3d7a20b8a76e17bb8a3d904
Artem Borisovskiy <bytefu@gmail.com>
2011-05-17 18:03:12 +0300

    Fixed indentation in LineDriver library to 2 spaces (Arduino style)

--- LineDriver.cpp
+++ LineDriver.cpp
@@ -26,23 +26,23 @@ DefaultLineDriver DefaultLineDriver::g_instance;
 
 void DefaultLineDriver::lineConfig(uint8_t pin, uint8_t mode)
 {
-    pinMode(pin, mode);
+  pinMode(pin, mode);
 }
 
 
 void DefaultLineDriver::lineWrite(uint8_t pin, uint8_t value)
 {
-    digitalWrite(pin, value);
+  digitalWrite(pin, value);
 }
 
 
 uint8_t DefaultLineDriver::lineRead(uint8_t pin)
 {
-    return digitalRead(pin);
+  return digitalRead(pin);
 }
 
 
 DefaultLineDriver* DefaultLineDriver::getInstance()
 {
-    return &g_instance;
+  return &g_instance;
 }
--- LineDriver.h
+++ LineDriver.h
@@ -25,22 +25,22 @@
 class LineDriver
 {
 public:
-    virtual void lineConfig(uint8_t pin, uint8_t mode) = 0;
-    virtual void lineWrite(uint8_t pin, uint8_t value) = 0;
-    virtual uint8_t lineRead(uint8_t pin) = 0;
+  virtual void lineConfig(uint8_t pin, uint8_t mode) = 0;
+  virtual void lineWrite(uint8_t pin, uint8_t value) = 0;
+  virtual uint8_t lineRead(uint8_t pin) = 0;
 };
 
 class DefaultLineDriver: public LineDriver
 {
 public:
-    virtual void lineConfig(uint8_t pin, uint8_t mode);
-    virtual void lineWrite(uint8_t pin, uint8_t value);
-    virtual uint8_t lineRead(uint8_t pin);
+  virtual void lineConfig(uint8_t pin, uint8_t mode);
+  virtual void lineWrite(uint8_t pin, uint8_t value);
+  virtual uint8_t lineRead(uint8_t pin);
 
-    static DefaultLineDriver* getInstance();
+  static DefaultLineDriver* getInstance();
 
 private:
-    static DefaultLineDriver g_instance;
+  static DefaultLineDriver g_instance;
 };
 
 #endif // LINEDRIVER_H_
