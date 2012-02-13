6a3eb0ad689ad73f9eb05f2d64fcf2fa0b300f7d
Artem Borisovskiy <bytefu@gmail.com>
2012-02-13 12:38:52 +0300

    Arduino 1.x compatibility fixes.

--- LineDriver.cpp
+++ LineDriver.cpp
@@ -19,7 +19,12 @@
 
 #include "LineDriver.h"
 #include <inttypes.h>
-#include "WProgram.h"
+
+#if defined(ARDUINO) && ARDUINO >= 100
+  #include "Arduino.h"
+#else
+  #include "WProgram.h"
+#endif
 
 DefaultLineDriver DefaultLineDriver::g_instance;
 
